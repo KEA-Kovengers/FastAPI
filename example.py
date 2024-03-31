22c0bf2d6a07a585388616b386740933191e53b5
ParkHyunseo <sspp8585@icloud.com>
2024-03-31 22:57:19 +0900

    initial commit

---
+++ example.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from PyKakao import Karlo
+api = Karlo(service_key = os.getenv("KAKAO_RESTAPI_KEY"))
+
+def image_create(text):
+    # 프롬프트에 사용할 제시어
+    # text = "Cute magical flyng cat, soft golden fur, fantasy art drawn by Pixar concept artist, Toy Story main character, clear and bright eyes, sharp nose"
+
+    # 이미지 생성하기 REST API 호출
+    img_dict = api.text_to_image(text, 1)
+
+    # 생성된 이미지 정보
+    img_str = img_dict.get("images")[0].get('image')
+    print(img_str)
+
+    return img_str
+
+# import requests
+# import shutil
+
+# # 이미지 다운로드할 URL
+# image_url = img_str
+
+# # GET 요청을 보내서 이미지 다운로드
+# response = requests.get(image_url, stream=True)
+
+# # 요청이 성공적인지 확인
+# if response.status_code == 200:
+#     # 파일로 저장할 경로 지정
+#     file_path = "downloaded_image.webp"
+#     # 스트림을 이용해 이미지 데이터를 파일로 저장
+#     with open(file_path, 'wb') as f:
+#         response.raw.decode_content = True
+#         shutil.copyfileobj(response.raw, f)
+#     print("이미지가 성공적으로 저장되었습니다:", file_path)
+# else:
+#     print("이미지를 다운로드할 수 없습니다.")
+
---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from typing import Optional
+import uvicorn
+from fastapi import FastAPI
+app = FastAPI()
+
+from pydantic import BaseModel
+from example import image_create
+
+# 사진을 요청하기 위해서 작성한 텍스트를 받기 위한 클래스
+class Content(BaseModel):
+    content: Optional[str] = None
+
+# 이미지 생성을 요청하기 위해 호출하는 함수
+# post 방식을 통해서 content: text 식으로 API를 호출하고
+# image_url을 통해서 사진을 돌려받는다.
+@app.post("/create/")
+async def create_item(item: Content):
+    image_url = image_create(item.content)
+    return {"content": image_url}
+
+# uvicorn
+if __name__ == '__main__' :
+    uvicorn.run('main:app', host='0.0.0.0', port=os.getenv('PORT'), access_log=True, reload=True, reload_dirs='./main.py')
