44afa0583cff08283abfe5aa2fc7855af852fdcb
ParkHyunseo <sspp8585@icloud.com>
2024-04-08 16:20:24 +0900

    Added Dockerfile

--- main.py
+++ main.py
@@ -1,47 +1,66 @@
+from http.client import HTTPException
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-from typing import Optional
+from typing import List, Optional, Union
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, status
 app = FastAPI()
 
 from pydantic import BaseModel
 from utils.karlo_service import image_create
 from utils.gemini_service import find_typo, generate_hashtag, generate_text
+from utils.jwt_service import encode, decode
 
 import pymongo
+
+import redis
+conn = redis.Redis()
 
 # MongoDB에 연결할 클라이언트 생성
 client = pymongo.MongoClient(os.getenv("MONGO_URL"))
 db = client[os.getenv("CLUSTER_NAME")]
 
-# 사진을 요청하기 위해서 작성한 텍스트를 받기 위한 클래스
-class Content(BaseModel):
-    content: Optional[str] = None
-
-# 이미지 생성을 요청하기 위해 호출하는 함수
-# post 방식을 통해서 content: text 식으로 API를 호출하고
-# image_url을 통해서 사진을 돌려받는다.
-@app.post("/create")
-def create_item(item: Content):
-    image_url = image_create(item.content)
-    return {"content": image_url}
-
 @app.get("/test")
 def text():
-    # collection = db["test"]
-    # new_document = {
-    #     "name": "count_eeho",
-    #     "age": 30,
-    #     "city": "Seoul"
-    # }
-    # cursor = collection.insert_one(new_document)
+    collection = db["test"]
+    new_document = {
+        "name": "count_eeho",
+        "age": 30,
+        "city": "Seoul"
+    }
+    cursor = collection.insert_one(new_document)
     # 첫 번째 결과 가져오기
     # first_document = cursor.next()
     # print(first_document)
     return "123"
+
+# api/post/check?postid=2
+@app.get("/api/post/check")
+def post_check(postid: int = 0, token: Union[str, None] = Header(default=None)):
+    if postid == 0:
+        return status.HTTP_502_BAD_GATEWAY
+    else:
+        text = decode(token)['id'] + '_' + str(postid)
+        conn.set(text, '1')
+        return True
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+
+# test
+@app.get("/api/get/token")
+def get_token():
+    return encode('660dff3450c489e4d36312eb')
 
 # uvicorn
 if __name__ == '__main__' :
---
+++ utils/jwt_service.py
@@ -0,0 +1,33 @@
+import datetime
+import jwt
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+def encode(id):
+    # 토큰 만료 시간 설정 (예: 현재 시간으로부터 1시간 후)
+    expiration_time = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+
+    # 토큰 생성
+    payload = {
+        'id': id,
+        'username': 'example_user',
+        'exp': expiration_time
+    }
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+    print("Encoded Token:", token)
+    return token
+
+def decode(token):
+    # 토큰 해독
+    try:
+        decoded_payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        print("Decoded Payload:", decoded_payload)
+        return decoded_payload
+    except jwt.ExpiredSignatureError:
+        print("Token has expired.")
+        return False
+    except jwt.InvalidTokenError:
+        print("Invalid token.")
+        return False
